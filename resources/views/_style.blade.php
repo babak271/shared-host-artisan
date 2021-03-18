7d4a9b3479b5dfd1ab25f05b94c72c19b2751dab
babak ashrafi <ashrafiseyedbabak@gmail.com>
2021-03-19 01:10:26 +0330

    Add some styles to index page

--- resources/views/_style.blade.php
+++ resources/views/_style.blade.php
@@ -1,1 +1,41 @@
-@include('_normal_style')
+<style>
+    .flex-container {
+        height: 100%;
+        padding: 0;
+        margin: 0;
+        display: -webkit-box;
+        display: -moz-box;
+        display: -ms-flexbox;
+        display: -webkit-flex;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .row {
+        width: auto;
+    }
+
+    .mr-5{
+        margin-top: 5rem;
+    }
+
+    .flex-item {
+        background-color: #06182b1f;
+        margin: 3rem;
+        line-height: 3rem;
+        color: white;
+        font-size: 1.5rem;
+        text-align: center;
+    }
+    .grid-container {
+        display: grid;
+        grid-template-columns: 20rem;
+        grid-gap: 0.5rem;
+        border-radius: 1rem;
+    }
+    a{
+        text-decoration: none;
+        color: rgba(13,5,47);
+    }
+</style>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,16 +5,28 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @include('_style')
+    @include('shared_host_artisan::_normal_style')
+    @include('shared_host_artisan::_style')
     <title>@lang('shared_host_artisan::text.index_page_title')</title>
 </head>
 <body>
-<div><a href="{{ route('share_host_artisan.clear_cache') }}">@lang('shared_host_artisan::text.clear_cache')</a></div>
-<div><a href="{{ route('share_host_artisan.clear_route') }}">@lang('shared_host_artisan::text.clear_route')</a></div>
-<div><a href="{{ route('share_host_artisan.cache_route') }}">@lang('shared_host_artisan::text.cache_route')</a></div>
-<div><a href="{{ route('share_host_artisan.clear_view') }}">@lang('shared_host_artisan::text.clear_view')</a></div>
-<div><a href="{{ route('share_host_artisan.cache_view') }}">@lang('shared_host_artisan::text.cache_view')</a></div>
-<div><a href="{{ route('share_host_artisan.clear_config') }}">@lang('shared_host_artisan::text.clear_config')</a></div>
-<div><a href="{{ route('share_host_artisan.cache_config') }}">@lang('shared_host_artisan::text.cache_config')</a></div>
+<div class="flex-container">
+    <div class="row mr-5">
+        <div class="grid-container flex-item">
+            <div><a href="{{ route('share_host_artisan.clear_cache') }}">@lang('shared_host_artisan::text.clear_cache')</a>
+            </div>
+            <div><a href="{{ route('share_host_artisan.clear_route') }}">@lang('shared_host_artisan::text.clear_route')</a>
+            </div>
+            <div><a href="{{ route('share_host_artisan.cache_route') }}">@lang('shared_host_artisan::text.cache_route')</a>
+            </div>
+            <div><a href="{{ route('share_host_artisan.clear_view') }}">@lang('shared_host_artisan::text.clear_view')</a></div>
+            <div><a href="{{ route('share_host_artisan.cache_view') }}">@lang('shared_host_artisan::text.cache_view')</a></div>
+            <div><a href="{{ route('share_host_artisan.clear_config') }}">@lang('shared_host_artisan::text.clear_config')</a>
+            </div>
+            <div><a href="{{ route('share_host_artisan.cache_config') }}">@lang('shared_host_artisan::text.cache_config')</a>
+            </div>
+        </div>
+    </div>
+</div>
 </body>
 </html>
